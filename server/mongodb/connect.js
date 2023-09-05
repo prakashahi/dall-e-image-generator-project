30260c7050aa06f7508f0fc9fb2d3f8200d54902
Prakash Shahi <learn.prakashshahi@gmail.com>
2023-09-05 22:56:08 +0545

    first commit

---
+++ server/mongodb/connect.js
@@ -0,0 +1,12 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("MongoDB connected!"))
+    .catch((err) => console.log(err));
+};
+
+export default connectDB;
